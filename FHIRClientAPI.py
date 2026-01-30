6ba6ff730a35f6d34d38e5d332cecbc13ae0be18
Manjunath Madhareddy <rmmanju@gmail.com>
2026-01-30 15:18:16 +0400

    Added root access

--- FHIRClientAPI.py
+++ FHIRClientAPI.py
@@ -4,6 +4,9 @@ from fhir.resources.fhirtypes import PatientType
 from fhir.resources.fhirtypes import HumanNameType
 from fhir.resources.identifier import Identifier
 from fhir.resources.fhirtypes import IdentifierType
+from fhir.resources.encounter import Encounter
+
+
 from pydantic import BaseModel
 
 
@@ -12,14 +15,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 FHIR_SERVER_URL = "https://hapi.fhir.org/baseR4/Patient" # Replace with your server URL
-MY_API_URL = "https://api-development-n7r3.onrender.com/get_patient/53888681"
+MY_API_URL = "https://api-development-n7r3.onrender.com/get_patient/"
 
 
 # ... create a Patient resource
 patient_name = HumanName(family='Manjunath', given=['Maddareddy'])
-patient = Patient(name=[patient_name], gender='male', birthDate='1984-06-01',identifier=[Identifier(system='http://hospital.smarthealthit.org', value='547414464')])
-
-
+patient = Patient(
+    name=[patient_name], 
+    gender='male',
+    birthDate='1984-06-01',
+    identifier=[Identifier(system='http://hospital.smarthealthit.org',
+    value='547414464')],
+    maritalStatus = {'coding': [{'system': 'http://terminology.hl7.org/CodeSystem/v3-MaritalStatus', 'code': 'M', 'display': 'Married'}]},
+    active=True,
+    contact=   [{'relationship': 
+               [{'coding': 
+               [{'system': 'http://terminology.hl7.org/CodeSystem/v2-0131', 'code': 'E', 'display': 'Emergency'}]}]}     ])
+encounter = Encounter(status = 'finished')
 patient_json = patient.model_dump_json()
 
 @app.post("/create_patient/")
@@ -31,7 +43,7 @@ def create_patient():
 @app.get("/get_patient/{patient_id}")
 def get_patient(patient_id: str):
     #response = requests.get(f"{FHIR_SERVER_URL}/{patient_id}", headers={"Content-Type": "application/fhir+json"})
-    response = requests.get(f"{MY_API_URL}", headers={"Content-Type": "application/fhir+json"})
+    response = requests.get(f"{MY_API_URL}/{patient_id}", headers={"Content-Type": "application/fhir+json"})
     if response.status_code == 200:
         patient_instance = Patient.model_validate_json(response.text)
         print("Patient ID:", patient_instance.identifier[0].value)
